0c9a368a3eb35d58e0bd66c46512ebae447be9ad
A\shiao <shiaotian206@gmail.com>
2023-07-22 23:54:15 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,38 @@
+export const products = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens',
+      category: "家具", 
+      stock: 5,
+      manufacturer: "エメラルド",
+      coverImage:'./assets/img/phone-cover.jpg' 
+    },
+    {
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras',
+      category: "家具", 
+      stock: 5,
+      manufacturer: "エメラルド", 
+      coverImage:'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
+    },
+    {
+      name: 'Phone Standard',
+      price: 299,
+      description: 'xxx',
+      category: "家具", 
+      stock: 5,
+      manufacturer: "エメラルド", 
+    },
+    {
+      "name": "電気スタンド", 
+      "category": "家具", 
+      "manufacturer": "エメラルド", 
+      "image_path": "/images/stand.jpg",
+      "price": 1500, 
+      "stock": 5,
+      "description": "とても明るい"
+   }
+  ];
+  
